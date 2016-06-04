aced7f8b20b54cbb1fdff4bff62d89f9a94e738e
watchbot <watchbot@>
2016-06-04 17:37:05 -0400

    Uses database to find unique unposted image for use

--- watchbot.py
+++ watchbot.py
@@ -1,7 +1,9 @@
 import tweepy
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta, date, time
+import cookielib
 import hashlib
+import logging
 import os
 import re
 import string
@@ -9,12 +11,17 @@ import urllib2
 import sqlite3
 from watchconfig import *  # import config
 
+class NoRedirection(urllib2.HTTPErrorProcessor):
+    def http_response(self, request, response):
+        return response
+    https_response = http_response
+
 def tweet_update(status, filename=None):
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
     if not status and not filename:
-        print "Tweet error"
+        logging.error("Tweet error")
     else:
         if filename:
             api.update_with_media(filename, status)
@@ -29,7 +36,7 @@ def google_search_url(lead_string, site_string):
     return search_url.lower()
 
 def exec_google_search(url):
-    #print url
+    logging.debug(url)
     header = {'User-Agent': user_agent}
     htmlresp = BeautifulSoup(urllib2.urlopen(urllib2.Request(url,headers=header)))
     imagesresp = [a['href'] for a in htmlresp.find_all("a", {"href": re.compile("jpg")})]
@@ -41,13 +48,40 @@ def parse_images(images):
         try:
             output = each.split('=')[1]
             output = output.split('&')[0]
-            possible_images.append(output)
+            if "photobucket" not in output:
+                possible_images.append(output)
         except:
-            output = None
+            logging.error("Split error")
 
     return possible_images
 
-def capture_image(url):
+def find_unused_image(image_urls, conn):
+    db = conn.cursor()
+    image_url = None
+    for each in image_urls:
+        logging.debug(each + "?")
+        db.execute("select rowid from images where url = ?", (each,))
+        rowid = db.fetchall()
+        if len(rowid) == 0:
+            image_url = each
+            break
+
+    return image_url
+
+def capture_image_to_file(url):
+    cj = cookielib.CookieJar()
+    opener = urllib2.build_opener(NoRedirection, urllib2.HTTPCookieProcessor(cj))
+    response = opener.open(url) #, urlencode(data))
+    if response.code != 200:
+        if response.code == 302 or response.code == 301:
+            url = response.headers['Location']
+            logging.debug("Redirected to " + url)
+        else:
+            logging.error("error fetching")
+            logging.error(response.code)
+            logging.error(response.headers)
+            return None
+
     raw_img = urllib2.urlopen(url).read()
     filename = hashlib.sha256(raw_img).hexdigest() + ".jpg"
     f = open(filename, 'wb')
@@ -68,24 +102,20 @@ if os.path.getsize(database_file)<100:
 
 images = exec_google_search(google_search_url(search_text, search_site))
 image_urls = parse_images(images)
-print image_urls
+logging.debug(image_urls)
 
-#TODO: adjust code to find one a pic not used in image_urls
-db.execute("select rowid from images where url = ?", (image_urls[0], ))
-rowid = db.fetchall()
-if len(rowid)==0:
-    print "not found"
+image_url = find_unused_image(image_urls, conn)
+logging.debug(image_url)
+
+if image_url:
+    image_file = capture_image_to_file(image_url)
+    logging.debug(image_file)
+    logging.debug(datetime.now())
+
+    db.execute("insert into images values (?, ?, ?)", (image_file, image_url, datetime.now()))
+    conn.commit()
+    #tweet_update("", image_file)
 else:
-    print "found"
-image_url = image_urls[0]
-print image_url
+    logging.error("Could not find a unique image")
 
-image_file = capture_image(image_url)
-print image_file
-print datetime.now()
-
-db.execute("insert into images values (?, ?, ?)", (image_file, image_url, datetime.now()))
-conn.commit()
 conn.close()
-
-#tweet_update("", image_file)
